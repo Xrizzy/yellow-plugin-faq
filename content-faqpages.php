ad8a5e6946f950b194cb827fa3e0decc03abb407
Xrizzy <xrizzy@outlook.com>
2018-01-05 18:41:28 +0100

    Update content-faqpages.php

--- content-faqpages.php
+++ content-faqpages.php
@@ -10,7 +10,7 @@
 <?php $sectionNew = htmlspecialchars(strtoupperu(substru($page->get("title"), 0, 1))) ?>
 <?php endif ?>
 <?php if($section != $sectionNew) { $section = $sectionNew; echo "</ul><h2>$section</h2><ul>\r\n"; } ?>
-<li><a href="<?php echo $page->getLocation() ?>"><?php echo $page->getHtml("title") ?></a></li>
+<li><a href="<?php echo $page->getLocation(true) ?>"><?php echo $page->getHtml("title") ?></a></li>
 <?php endforeach ?>
 </ul>
 <?php $yellow->snippet("pagination", $yellow->page->getPages()) ?>
